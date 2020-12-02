bf8f6a78378e68e13e872deae83aef3ab565b280
stanislavvitvitskiy <stanislav.i.vitvitskiy@gmail.com>
2020-12-02 14:01:35 -0800

    feat: add initial version of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import {SignIn, ChatRoom, SignOut} from './SignIn/SignIn'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {useCollectionData} from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBwOjX_KwRD_dCqTjFirn0KoUsomisXhHw",
+    authDomain: "chat-ad599.firebaseapp.com",
+    databaseURL: "https://chat-ad599.firebaseio.com",
+    projectId: "chat-ad599",
+    storageBucket: "chat-ad599.appspot.com",
+    messagingSenderId: "584079205141",
+    appId: "1:584079205141:web:e92e8013fbb19a51d37f1c",
+    measurementId: "G-28D5DR3CH1"
+})
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
 function App() {
+  const [user] = useAuthState(auth);
+  console.log("USER: ", user)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <section>
+            {user ? <ChatRoom /> : <SignIn />}
+            <SignOut/>
+        </section>
     </div>
   );
 }
