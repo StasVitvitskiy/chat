19bf3e4266e8e8cba3e988ccc9429500c8cd4799
stanislavvitvitskiy <stanislav.i.vitvitskiy@gmail.com>
2020-12-08 10:18:27 -0800

    feat: sign up implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import './App.css';
+import {auth} from './firebase'
+import {AuthProvider} from "./contexts/AuthContext";
 
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
 import {SignIn, ChatRoom, SignOut} from './SignIn/SignIn'
 import {useAuthState} from 'react-firebase-hooks/auth'
-import {useCollectionData} from "react-firebase-hooks/firestore";
-
-firebase.initializeApp({
-    apiKey: "AIzaSyBwOjX_KwRD_dCqTjFirn0KoUsomisXhHw",
-    authDomain: "chat-ad599.firebaseapp.com",
-    databaseURL: "https://chat-ad599.firebaseio.com",
-    projectId: "chat-ad599",
-    storageBucket: "chat-ad599.appspot.com",
-    messagingSenderId: "584079205141",
-    appId: "1:584079205141:web:e92e8013fbb19a51d37f1c",
-    measurementId: "G-28D5DR3CH1"
-})
-
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+import {SignUp} from './SignUp/SignUp'
+import {Container} from 'react-bootstrap'
 
 function App() {
   const [user] = useAuthState(auth);
@@ -28,10 +14,17 @@ function App() {
 
   return (
     <div className="App">
-        <section>
-            {user ? <ChatRoom /> : <SignIn />}
-            <SignOut/>
-        </section>
+        <AuthProvider>
+            <section>
+                {user ? <ChatRoom /> :
+                    <Container className="d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
+                        <div className="w-100" style={{maxWidth: "400px"}}>
+                            <SignIn /> <SignUp />
+                        </div>
+                    </Container>}
+                <SignOut/>
+            </section>
+        </AuthProvider>
     </div>
   );
 }
