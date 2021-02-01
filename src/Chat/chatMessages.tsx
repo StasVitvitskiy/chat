ee7a3022d1119834f0792680afd65ac2f9d0dba4
stanislavvitvitskiy <stanislav.i.vitvitskiy@gmail.com>
2021-02-01 09:03:36 -0800

    feat: add scroll to the last message

--- src/Chat/chatMessages.tsx
+++ src/Chat/chatMessages.tsx
@@ -1,10 +1,9 @@
 import React, {PureComponent} from "react";
-import {Box} from "@material-ui/core";
+import {Box, debounce} from "@material-ui/core";
 import {connect} from "react-redux";
 import {ChatState, WithChatState} from "./chatReducer";
 import { WithUserState} from "../User";
 import firebase from "firebase";
-import {format } from 'date-fns'
 import {ChatMessage} from "./chatMessage";
 
 export const ChatMessages = connect(
@@ -14,6 +13,28 @@ export const ChatMessages = connect(
     })
 )(
     class extends PureComponent<ChatState & {currentUser: firebase.User | null | undefined}> {
+        componentDidMount() {
+            window.addEventListener("resize", this.onWindowResize);
+        }
+
+        componentDidUpdate() {
+            this.scrollToLastMessage()
+        }
+
+        componentWillUnmount() {
+            window.removeEventListener("resize", this.onWindowResize);
+        }
+        scrollToLastMessage = () => {
+            if(this.messagesContainerRef.current) {
+                this.messagesContainerRef.current.scrollTop = this.messagesContainerRef.current.scrollHeight
+            }
+        }
+        onWindowResize = debounce(() => {
+            this.scrollToLastMessage()
+        }, 500)
+
+        messagesContainerRef = React.createRef<HTMLDivElement>()
+
         render() {
             const isChatOpen = Boolean(this.props.id);
             const {user1,user2, currentUser} = this.props
@@ -23,7 +44,14 @@ export const ChatMessages = connect(
                 }
             }
             return(
-                <Box width='100%' height='100%' bgcolor={isChatOpen ? "white" : "#b7b7b7"}>
+                <Box
+                    // @ts-ignore
+                    ref={this.messagesContainerRef}
+                    maxHeight='100%' overflow='scroll'
+                    width='100%'
+                    height='100%'
+                    bgcolor={isChatOpen ? "white" : "#b7b7b7"}
+                >
                     {this.props.messages.map((el) => {
                         return <ChatMessage
                             key={el.id}
